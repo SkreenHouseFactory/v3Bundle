89d5a8698ccb5ab24c4b891f756512b8b2070d7f
bbergstorm <benoit@myskreen.com>
2012-06-08 13:25:46 +0200

    session

--- Api/ApiManager.php
+++ Api/ApiManager.php
@@ -2,11 +2,33 @@
 
 namespace SkreenHouseFactory\v3Bundle\Api;
 
+use Guzzle\Http\Client;
+
+
 class ApiManager
 {
+  protected $api_base = null;
+  protected $api_format = null;
 
-  public function __construct($api_url, $api_format) {
+  public function __construct($api_base, $api_format) {
+    $this->api_base   = $api_base;
+    $this->api_format = $api_format;
+  }
+
+  public function fetch($url, $params = array(), $method = 'GET') {
     
+    $client = new Guzzle\Http\Client($this->api_base.$url.'.'.$this->api_format, $params);
+    
+    switch ($method) {
+      case 'POST':
+        $reponse = $client->send($client->post());
+      break;
+      case 'GET':
+        $reponse = $client->send($client->get());
+      break;
+    }
+
+    return json_decode((string)$reponse);
   }
 
   /**
@@ -14,5 +36,6 @@ class ApiManager
   */
   public function getObject($name, $id) {
     
+    return $this->fetch($name.'/'.$id)
   }
 }
