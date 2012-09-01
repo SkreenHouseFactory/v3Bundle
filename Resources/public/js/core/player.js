cd3cb9ad52d9d76af32f9ce58a108aa50cbcbe94
bbergstorm <benoit@myskreen.com>
2012-09-01 22:10:35 +0200

    flowplayer

--- Resources/public/js/core/player.js
+++ Resources/public/js/core/player.js
@@ -4,12 +4,19 @@ Player = {
   elmt: null,
   timeout: null,
   timeoutdelay: 5000,
-  type: 'flash',
+  type: 'flowplayer',
   is_playing: false,
   //player
   init: function(elmt) {
     this.elmt = elmt;
     this.elmt.empty();
+
+    var idevice = /ip(hone|ad|od)/i.test(navigator.userAgent);
+    var flashversion = flashembed.getVersion()[0];
+    if (idevice || !flashversion) {
+      this.type = 'html5';
+    }
+
   },
   load: function(trigger) {
     var self = this;
@@ -81,22 +88,65 @@ Player = {
     iframe.show().css('height', ($(window).height() - 100) + 'px');
     $('#top-nav').collapse('hide');
   },
-  play: function(player) {
+  play: function(player, callback) {
     this.is_playing = true;
 
     switch(this.type) {
+
+      /*
+        http://flowplayer.org/documentation/api/player.html
+        
+        100	Plugin initialization failed
+        200	Stream not found
+        201	Unable to load stream or clip file
+        202	Provider specified in clip is not loaded
+        300	Player initialization failed
+        301	Unable to load plugin
+        302	Error when invoking plugin external method
+        303	Failed to load resource such as stylesheet or background image
+      */
+      case 'html5':
+      case 'flowplayer':
+
+        var flashArgs = {
+              "src": player.swf || "http://releases.flowplayer.org/swf/flowplayer-3.2.7.swf",
+              "wmode":"opaque",
+        	    "allowfullscreen":"true",
+              "cachebusting": "false",
+        	    "width": player.width || "100%",
+        	    "height": player.height || "100%",
+        	    "allowscriptaccess":"always"
+        };
+
+        flowArgs = player.config ? JSON.parse(player.config) : {"clip":{"url":player.url}};
+        flowArgs.onMouseOver = function() {};
+        flowArgs.onMouseOut = function() {};
+        flowArgs.onBegin = function() {};
+        flowArgs.onFinish = function() {
+          if (typeof callback != 'undefined') {
+            callback();
+          }
+        }
+        flowArgs.onError = function(errorCode,errorMessage) {
+          console.error('Player.flowplayer', player, errorCode, errorMessage);
+        };
+
+        if (this.type == 'html5') {
+          console.warn('Player.flowplayer', 'iOs/noflash', player, flowArgs);
+          Player.elmt.css('height', document.body.clientHeight);
+          $f(this.elmt.attr('id'), flashArgs, flowArgs).ipad({simulateiDevice: true, controls: false});
+        } else {
+          console.warn('Player.flowplayer', 'flash:v' + flashversion, player, flowArgs);
+          $f(this.elmt.attr('id'), flashArgs, flowArgs);
+        }
+      break;
       case 'flash':
         this.elmt.html(player);
       break;
       case 'android':
-        //Webview.postMessage(['player', 'launch', 'rtsp://v5.cache1.c.youtube.com/CjYLENy73wIaLQnhycnrJQ8qmRMYESARFEIJbXYtZ29vZ2xlSARSBXdhdGNoYPj_hYjnq6uUTQw=/0/0/0/video.3gp']);
-        //Webview.postMessage(['player', 'launch', 'http://benoit.myskreen.typhon.net/48316914.mp4']);
         if (typeof Webview != 'undefined') {
           Webview.postMessage(['player', 'launch', player.url]);
         }
-      break;
-      case 'html5':
-        this.elmt.html('loading embed html5 ...' + embed.url);
       break;
       case 'iframe':
         this.elmt.html('<iframe src="' + embed.url + '" frameborder="0"></iframe>');
@@ -104,26 +154,49 @@ Player = {
     }
   },
   pause: function(player) {
-    if (this.type == 'android' && typeof Webview != 'undefined') {
-      Webview.postMessage(['player', 'pause']);
+    switch(this.type) {
+      case 'android':
+        if (typeof Webview != 'undefined') {
+          Webview.postMessage(['player', 'pause']);
+        }
+      break;
+      case 'html5':
+      case 'flowplayer':
+        $f(this.elmt.attr('id')).pause();
+      break;
     }
   },
   resume: function(player) {
-    if (this.type == 'android' && typeof Webview != 'undefined') {
-      Webview.postMessage(['player', 'play']);
+    switch(this.type) {
+      case 'android':
+        if (typeof Webview != 'undefined') {
+          Webview.postMessage(['player', 'play']);
+        }
+      break;
+      case 'html5':
+      case 'flowplayer':
+        $f(this.elmt.attr('id')).resume();
+      break;
     }
   },
   stop: function(player) {
-    this.is_playing = false;
-    if (this.type == 'android' && typeof Webview != 'undefined') {
-      Webview.postMessage(['player', 'stop']);
+    switch(this.type) {
+      case 'android':
+        if (typeof Webview != 'undefined') {
+          Webview.postMessage(['player', 'stop']);
+        }
+      break;
+      case 'html5':
+      case 'flowplayer':
+        $f(this.elmt.attr('id')).stop();
+      break;
     }
 
     if (this.elmt.data('playing-id')) {
       this.elmt.data('playing-id', '');
     }
   },
-  playProgram: function(id) {
+  playProgram: function(id, callback) {
     var self = this;
     return API.query('GET',
                      'player/program/' + id + '.json',
@@ -140,7 +213,7 @@ Player = {
                       if (!media.player || typeof media.player == 'undefined') {
                         return false;
                       }
-                      self.play(media.player);
+                      self.play(media.player, callback);
                       self.elmt.data('playing-id', id);
 
                       return true;
