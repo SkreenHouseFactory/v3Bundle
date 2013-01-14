e3a94fb58a8f86f1308ee2717bb91d463babe67f
bbergstorm <benoit@myskreen.com>
2013-01-15 00:20:30 +0100

    routing channel - cocon

--- Controller/ContentController.php
+++ Controller/ContentController.php
@@ -188,10 +188,11 @@ class ContentController extends Controller
       //echo $api->url;
         //echo $request->getPathInfo().' != '.$datas->seo_url.' => '.($request->getPathInfo() != $datas->seo_url);exit();
         if ($request->getPathInfo() != '/' . $datas->seo_url . '/' &&
+            $request->getPathInfo() != '/' . $datas->seo_url . '/' . $request->get('format') . '/' &&
             $request->getPathInfo() != '/' . $datas->seo_url . '/page-' . $request->get('page') . '/' &&
             $request->getPathInfo() != '/' . $datas->seo_url . '/' . $request->get('facet') . '/'
             ) {
-          //echo 'redirect '.$request->getPathInfo().' != /'.$datas->seo_url.'/ => '.($request->getPathInfo() != $datas->seo_url);exit();
+          //echo "\n".'redirect '.$request->getPathInfo().' != /'.$datas->seo_url.'/ => '.($request->getPathInfo() != $datas->seo_url);exit();
           return $this->redirect('/'.$datas->seo_url.'/');
         }
       $datas->picture = str_replace('150/200', '240/320', isset($datas->programs[0]) && is_object($datas->programs[0]) ? $datas->programs[0]->picture : null);
