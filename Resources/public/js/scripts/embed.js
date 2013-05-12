057e71383659cb5c6cf61efb5107f3a641e0cd67
bbergstorm <benoit@myskreen.com>
2013-05-12 19:15:07 +0200

    debug popin channel connecté avec follow

--- Resources/public/js/scripts/embed.js
+++ Resources/public/js/scripts/embed.js
@@ -3,4 +3,10 @@ $(document).ready(function(){
   $('#trigger').on('click', function(){
     $('.player-cover').fadeOut();
   })
+
+  if (navigator.userAgent.match(/iPhone|iPod|Android/gi)) {
+    console.log(['scripts/embed.js', 'iPhone|iPod|Android', 'default click']);
+    $('#trigger').trigger('click');
+  }
+  
 })
