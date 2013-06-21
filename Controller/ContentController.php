9872ff060431cc200b120056f44b28098e5f0e4b
bbergstorm <benoit@myskreen.com>
2013-06-21 12:46:42 +0200

    map cine

--- Controller/ContentController.php
+++ Controller/ContentController.php
@@ -34,7 +34,6 @@ class ContentController extends Controller
     */
     public function channelAction(Request $request)
     {
-
       $this->blockDomain($request);
 
       $api   = $this->get('api');
@@ -134,7 +133,7 @@ class ContentController extends Controller
           'formats' => array_combine(explode(';', $datas->facets_seo_url->format),explode(';', $datas->facets->format)),
           'categories' => array_combine(explode(';', $datas->facets_seo_url->category),explode(';', $datas->facets->category)),
           'subcategories' => array_combine(explode(';', $datas->facets_seo_url->subcategory),explode(';', $datas->facets->subcategory)),
-          'access' => explode(';', $datas->facets->access),
+          'access' => explode(';', isset($datas->facets->access) ? $datas->facets->access : null),
           'alpha_available' => explode(';', $datas->facets->alpha),
           'alpha'    => array(1,2,3,4,5,6,7,8,9,
                               'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
--- Resources/public/js/scripts/channel.js
+++ Resources/public/js/scripts/channel.js
@@ -17,6 +17,37 @@ $(document).ready(function(){
       $('[title="' + channel_name + ' Replay"]').parent().addClass('active');
     }
 
+    if ($('#map-container').length) {
+      var map;
+      var mapOptions = {
+        zoom: 16,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+      map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+      geocoder = new google.maps.Geocoder();
+      geocoder.geocode({ 'address': $('#map-canvas').data('adress')}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          map.setCenter(results[0].geometry.location);
+          //marker
+          var marker = new google.maps.Marker({
+           position: results[0].geometry.location,
+          	map: map,
+          	title: $('#map-canvas').data('name')
+          });
+          // fenêtre
+          var myWindowOptions = {
+          	content: '<h6>'+$('#map-canvas').data('name')+'</h6>'
+          };
+          var myInfoWindow = new google.maps.InfoWindow(myWindowOptions);
+          google.maps.event.addListener(marker, 'click', function() {
+          	myInfoWindow.open(map, marker);
+          });
+        } else {
+          console.error("Geocode was not successful for the following reason: " + status);
+        }
+      });
+    }
+
   // -- chaine
   } else if ($('#view-page').length) {
 
