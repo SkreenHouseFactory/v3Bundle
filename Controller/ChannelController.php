7f6c0c14f48ccad89fd36bc34cc7245fc37c1c8c
Thierry <thierry.zng@gmail.com>
2013-08-16 10:21:52 +0200

    pblv date & grid arrow

--- Controller/ChannelController.php
+++ Controller/ChannelController.php
@@ -177,7 +177,7 @@ class ChannelController extends Controller
          //'offers_type'=> 'plays'
        );
       $program = $api->fetch('program/3517970', $params);
-      //echo $api->url;
+      echo $api->url;
       $play = null;
       
       foreach( $program->offers->plays as $play){
@@ -185,7 +185,7 @@ class ChannelController extends Controller
           break;
         }
       }
-
+      $play->starttime = $play->starttime + 604800;
       foreach( $program->datas_offers->episodes as $episode) {
         if( isset($episode->title) && $episode->id == $play->episode_id ){
           break;
