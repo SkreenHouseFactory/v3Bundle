3ddcaff5a09b57e6a8c03cf90b70ba961f8ec464
marcmyskreen <marc@myskreen.com>
2013-08-28 19:25:25 +0200

    page chaines + playlist

--- Controller/ChannelController.php
+++ Controller/ChannelController.php
@@ -42,7 +42,7 @@ class ChannelController extends Controller
       'with_next_live' => !$request->get('format') && !$request->get('page') ? true : false,
       //'with_prev_live' => true,
       'with_description'  => true,
-      'channel_img_width' => 120,
+      'channel_img_width' => 80,
       'img_width' => 150,
       'img_height' => 200,
       'live_img_width' => 150,
