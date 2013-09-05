72168ad88ba011ad1a86a1095fd71fd14959f36b
bbergstorm <benoit@myskreen.com>
2013-09-05 09:42:14 +0200

    debug

--- Controller/MainController.php
+++ Controller/MainController.php
@@ -34,6 +34,9 @@ class MainController extends Controller
     public function headerAction(Request $request)
     {
       return $this->render('SkreenHouseFactoryV3Bundle:Main:_header.html.twig', array(
+        'home'=> $request->get('home'),
+        'route'=> $request->get('route'),
+        'q'=> $request->get('q')
       ));
     }
 
