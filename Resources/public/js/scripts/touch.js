37ad36745f523f134f066019d9b955dba0588973
bbergstorm <benoit@myskreen.com>
2013-09-17 18:10:53 +0200

    page fournisseur

--- Resources/public/js/scripts/touch.js
+++ Resources/public/js/scripts/touch.js
@@ -2,6 +2,7 @@
 $(document).ready(function(){
   //alert('touch:' + $('html').hasClass('touch'));
   if ($('html').hasClass('touch')) {
+    /*
     $('html.touch').on('touchstart', function(e){
         console.log('scripts/core/ui.js', 'touch trigger click');
         //alert('touch trigger click: ' + this.className);
@@ -10,22 +11,6 @@ $(document).ready(function(){
        }
        Player.stop();
     });
-    
-
-   // Changement du fonctionnement des boutons du header pour les appareils iOS/touch
-   $('.with-caret.tv-component').on('click',function(event) {
-     if( !$(this).parent().hasClass('open')){
-       $(this).siblings('.a-caret').trigger('click');
-       event.preventDefault();
-     }
-     else{
-       window.location = $(this).attr('href');
-     }
-   });
-   $('.a-caret').on('click', function(){
-     if(!$(this).parent().hasClass('open')){
-       Player.stop();
-     }
-   });
+    */
   }
 });
