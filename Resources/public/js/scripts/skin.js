f8e3f357e116ff36bb434643f2fd3b4ac8895163
Thierry <thierry.zng@gmail.com>
2013-09-20 16:19:54 +0200

    event

--- Resources/public/js/scripts/skin.js
+++ Resources/public/js/scripts/skin.js
@@ -7,12 +7,12 @@ var Skin;
 Skin = {
   initHome: function() {
     console.log('initSkinHome');
-    if( $('body').hasClass('home') && $('body').hasClass('skin')) {
+    if( ($('body').hasClass('home') || $('body').hasClass('home-v3-cine')) && $('body').hasClass('skin')) {
       $('.absolute').addClass('hide');
       $('body').prepend('<a class="background">');
       $('body').append('</a>');
       $('body .background').attr("href","http://www.myskreen.com/les-conquerants/");
-      $('#carousel-a-la-une .item a').each(function(){
+      $('#carousel-a-la-une .item a','#carousel-selection-cinema .item a').each(function(){
         var background_image=$(this).css('background-image');
         background_image = background_image.replace('/1500/', '/970/');
         console.log(background_image);
