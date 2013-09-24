1809e5f86de9ccc5a79e5d7aae0491b3fb439166
Thierry <thierry.zng@gmail.com>
2013-09-24 16:21:23 +0200

    fond conquérant

--- Resources/public/js/scripts/skin.js
+++ Resources/public/js/scripts/skin.js
@@ -12,9 +12,8 @@ Skin = {
     console.log('initSkinHome');
     if( ($('body').hasClass('home') || $('body').hasClass('home-cinema')) && $('body').hasClass('skin')) {
       $('.absolute').addClass('hide');
-      $('body').prepend('<a class="background">');
-      $('body').append('</a>');
-      $('body .background').attr("href","http://www.myskreen.com/les-conquerants/");
+      $('#view-home').prepend('<a class="background"></a>');
+      $('#view-home .background').attr("href","http://www.myskreen.com/les-conquerants/");
       $('#carousel-a-la-une .carousel-inner .item a , #carousel-selection-cinema .carousel-inner .item a').each(function(){
         var background_image = $(this).css('background-image');
         background_image = background_image.replace('/1500/', '/970/');
