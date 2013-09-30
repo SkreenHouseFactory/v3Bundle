2ea7291ca61b9085cd9efc13f5f3ce9d44e2bdfd
Thierry <thierry.zng@gmail.com>
2013-09-30 16:29:25 +0200

    debug

--- Resources/public/js/scripts/_default.js
+++ Resources/public/js/scripts/_default.js
@@ -46,27 +46,6 @@ $(document).ready(function(){
     Skhf.session.signout();
     return false;
   });
-  //alert nouveau visiteur
-  
-   if(!$.cookie('myskreen_new')){
-    $('#main .help-sprite-ms_btn_help').trigger('click');
-  }
-  $('#main .help-sprite-ms_btn_close').on('click', function(){
-    API.cookie('new','true');
-  });
-
-  //Warning IE7
-  if($('html').hasClass('lt-ie8')){
-    if(!$.cookie('myskreen_ie7')){
-      $('.lt-ie8-pop').toggleClass('hide');
-      $('.modal-backdrop.in').toggleClass('hide');
-    }
-    $('.lt-ie8-pop .ie-body .btn').on('click', function(){
-       API.cookie('ie7','true');
-       $('.lt-ie8-pop').toggleClass('hide');
-       $('.modal-backdrop.in').toggleClass('hide');
-    });
- }
   
 
   // new header nav bar
@@ -216,7 +195,37 @@ $(document).ready(function(){
     $('.help-sprite').toggleClass('help-sprite-ms_btn_close');
     $('.help-popin').toggleClass('hide');
     $('.modal-backdrop.in').toggleClass('hide');
+    API.cookie('new','true');
   });
+  
+  //alert nouveau visiteur
+  
+   if(!$.cookie('myskreen_new')){
+    $('#main .help-sprite-ms_btn_help').trigger('click');
+  }
+  $('#main .help-sprite-ms_btn_close').on('click', function(){
+    API.cookie('new','true');
+  });
+
+  //Warning IE7
+  if($('html').hasClass('lt-ie8')){
+    if(!$.cookie('myskreen_ie7')){
+      $('.lt-ie8-pop').toggleClass('hide');
+      $('.modal-backdrop.in').toggleClass('hide');
+      if( !$('.help-popin').hasClass('hide') ){
+        $('#main .help-sprite-ms_btn_close').trigger('click');
+        API.cookie('new',null);
+      }
+    }
+    $('.lt-ie8-pop .ie-body .btn').on('click', function(){
+       API.cookie('ie7','true');
+       $('.lt-ie8-pop').toggleClass('hide');
+       $('.modal-backdrop.in').toggleClass('hide');
+       if(!$.cookie('myskreen_new')){
+        $('#main .help-sprite-ms_btn_help').trigger('click');
+      }
+    });
+ }
 
    // -- beead
    if ($('body').hasClass('beead')) {
