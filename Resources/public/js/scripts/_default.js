601d1208e37c27fc82382d9b328ced2b7e590ac0
Thierry <thierry.zng@gmail.com>
2013-10-14 13:25:17 +0200

    debug

--- Resources/public/js/scripts/_default.js
+++ Resources/public/js/scripts/_default.js
@@ -203,7 +203,7 @@ $(document).ready(function(){
 
 
 /* END */
-  if( $('#channel60').length == 0){
+  if( $('#channel60').length == 0 && !$('#program-modal').length){
   //-- Script  pour le helper première visite
   $('.help-sprite.help-sprite-ms_btn_help').on('click', function () {
     $(this).toggleClass('help-sprite-ms_btn_help');
