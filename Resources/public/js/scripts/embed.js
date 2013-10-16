08cf3f9f9f020e58cfecd13168dd69900666448d
bbergstorm <benoit@myskreen.com>
2013-10-16 13:32:40 +0200

    embed nopub

--- Resources/public/js/scripts/embed.js
+++ Resources/public/js/scripts/embed.js
@@ -1,11 +1,11 @@
 if (!document.getElementsByClassName) {
-    document.getElementsByClassName=function(cn) {
-        var allT=document.getElementsByTagName('*'), allCN=[], i=0, a;
-        while(a=allT[i++]) {
-            a.className==cn ? allCN[allCN.length]=a : null;
-        }
-        return allCN
+  document.getElementsByClassName=function(cn) {
+    var allT=document.getElementsByTagName('*'), allCN=[], i=0, a;
+    while(a=allT[i++]) {
+        a.className==cn ? allCN[allCN.length]=a : null;
     }
+    return allCN
+  }
 }
 
 window.onload = function(){
