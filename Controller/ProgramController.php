101ffe5975b64075bb48026842a840c5a1308804
Thierry <thierry.zng@gmail.com>
2013-10-30 15:05:55 +0100

    debug

--- Controller/ProgramController.php
+++ Controller/ProgramController.php
@@ -142,7 +142,7 @@ class ProgramController extends Controller
           }
         }
 
-        echo $api->url;exit;
+        //echo $api->url;exit;
         if ($this->get('kernel')->getEnvironment() == 'dev' && 
             $request->get('debug')) {
           echo $api->url;
