3b5b566689e98723e1b53745bde6581bbe19590a
Thierry <thierry.zng@gmail.com>
2013-11-07 12:17:18 +0100

    code javascript pour desactiver les liens pour googlebot

---
+++ Resources/public/js/scripts/seo.js
@@ -0,0 +1,8 @@
+// remplace les liens non visible par google par des liens visibles
+
+$(document).ready(function(){
+	$('[data-link-to-replace]').each(function(){
+		var node = '<a href="'+ $(this).data('link') +'">'+$(this).html()+'</a>';
+		$(this).replaceWith(node);
+	});
+});
