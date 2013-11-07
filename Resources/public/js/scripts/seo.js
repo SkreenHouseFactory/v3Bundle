d0fd161364ec25cf30c0bfbe0104c8df1e6d844b
Thierry <thierry.zng@gmail.com>
2013-11-07 12:18:06 +0100

    code javascript pour desactiver les liens pour googlebot fix #54

--- Resources/public/js/scripts/seo.js
+++ Resources/public/js/scripts/seo.js
@@ -4,5 +4,6 @@ $(document).ready(function(){
 	$('[data-link-to-replace]').each(function(){
 		var node = '<a href="'+ $(this).data('link') +'">'+$(this).html()+'</a>';
 		$(this).replaceWith(node);
+    
 	});
 });
