e10f6ea25235b707e5e3f6bfae9e30d021068526
bbergstorm <benoit@myskreen.com>
2013-12-16 13:50:16 +0100

    autosuggest search

--- Resources/public/js/ui.js
+++ Resources/public/js/ui.js
@@ -988,7 +988,7 @@ UI = {
       function(data) {
        
         console.log('UI.typeahead', query, data);
-        if (data.channels || data.theaters || data.programs || data.persons || data.queue || data.categories || data.sagas) {
+        if (data.channels || data.theaters || data.programs || data.persons || data.queue || data.categories || data.sagas || data.packs) {
           var lis = new Array;
           var titles = new Array;
           typeahead.query = typeahead.$element.val();
@@ -1011,6 +1011,10 @@ UI = {
               case 'sagas':
                 var items = data[key];
                 titles[key] = 'Sagas';
+                break;
+              case 'packs':
+                var items = data[key];
+                titles[key] = 'Sélections';
                 break;
               case 'theaters':
                 var items = data[key];
@@ -1065,6 +1069,12 @@ UI = {
                      .find('a')
                      .html(typeahead.highlighter(item.name))
                     break;
+                  case 'packs':
+                    i.addClass('pack')
+                     .css('overflow','hidden')
+                     .find('a')
+                     .html(typeahead.highlighter(item.name))
+                    break;
                   case 'programs':
                     i.addClass('program')
                       .css('overflow','hidden')
@@ -1096,7 +1106,7 @@ UI = {
 
           //data.first().addClass('active')
           //var sort = Array('channels','theaters','real-channels','programs','persons','categories','queue');
-          var sort = Array('channels','programs','sagas','persons','theaters','categories','queue');
+          var sort = Array('queue','channels','programs','sagas','persons','theaters','categories','packs');
           for (key in sort) {
             if (lis[sort[key]]) {
               //console.log('UI.typeahead', key, data[key], typeahead.$menu);
