586ccab42104da2151b0abeb6128a3dc92c7f755
axel-myskreen <axel@myskreen.com>
2014-02-06 15:13:19 +0100

    fix console log ie9

--- Resources/public/js/scripts/modal.js
+++ Resources/public/js/scripts/modal.js
@@ -1,27 +1,34 @@
+  if(typeof console == 'undefined' || typeof console.log === 'undefined') {
+      console = {
+          log: function() {},
+          warn: function() {},
+          error: function() {}
+  }
+
   //////////// CALLBACKS ////////////////
   // -- session sync
   callbackSignin = function() {
     if(Skhf.session.user){
-      console.log('scripts/modal.js', 'callbackSignin', window.opener);
+      //console.log('scripts/modal.js', 'callbackSignin', window.opener);
       window.opener.document.location.reload();
       window.close();
     }
   }
   callbackInit = function(session) {
-    console.log('pas de session');
+    //console.log('pas de session');
     if(!session.uid){
       session.sync(function(){
-        console.log('modal.js', 'callbackInit', Skhf.session.uid);
+        //console.log('modal.js', 'callbackInit', Skhf.session.uid);
         $('#form_session_uid').val(Skhf.session.uid);
       });
     } else {
-      console.log('modal.js', 'callbackInit', Skhf.session.uid);
+      //console.log('modal.js', 'callbackInit', Skhf.session.uid);
       $('#form_session_uid').val(Skhf.session.uid);
     }
   }
   
 $(document).ready(function(){
-  console.log('modal.js', 'ready');
+  //console.log('modal.js', 'ready');
   if($('#close').length>0){
     //console.log('test close');
     //console.log('callbackSignin', callbackSignin);
