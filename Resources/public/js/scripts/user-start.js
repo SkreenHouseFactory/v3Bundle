b02e2ff2f7dfefdacfbac1428fb97bf036e5177a
axel-myskreen <axel@myskreen.com>
2014-03-13 18:28:25 +0100

    start listes

--- Resources/public/js/scripts/user-start.js
+++ Resources/public/js/scripts/user-start.js
@@ -2,6 +2,17 @@
 
 $(document).ready(function(){
 
+  $('.forms-container').scrollspy({ target: '.navbar-vertical' });
+
+  $('.timeline').affix({
+    offset: {
+      top: 275, 
+      bottom: 250
+    }
+  })
+  $('.timeline li').removeClass('active');
+  $('.timeline li:first-child').addClass('active');
+
   //forms
   $('form').on('submit', function(e){
     e.preventDefault();
@@ -22,7 +33,7 @@ $(document).ready(function(){
         console.log('scripts/user-start.js', 'callback', container);
         for (k in results) {
           title = typeof results[k].title != 'undefined' ? results[k].title : results[k].name;
-          container.append('<li>' + title + '<a data-id="'+results[k].id+'" rel="popover" data-placement="left" data-store-in-session="1" class="btn btn-suivre btn-plus fav-like" data-original-title="" title="">Ajouter à mes listes </a></li>')
+          container.append('<li class="clearfix">' + title + '<a data-id="'+results[k].id+'" rel="popover" data-placement="left" data-store-in-session="1" class="btn btn-suivre btn-plus fav-like pull-right" data-original-title="" title="">Ajouter à mes listes </a></li>')
         }
     });
 
