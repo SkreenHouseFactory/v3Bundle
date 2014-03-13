9af2bc1f541d606c4b4b21168cba6a1c92613967
bbergstorm <benoit@myskreen.com>
2014-03-13 14:12:35 +0100

    start

--- Resources/public/js/scripts/user-start.js
+++ Resources/public/js/scripts/user-start.js
@@ -2,4 +2,30 @@
 
 $(document).ready(function(){
 
+  //forms
+  $('form').on('submit', function(e){
+    e.preventDefault();
+    var self = $(this);
+    q = $(this).find('input[type=text]').val();
+    API.query(
+      'GET', 
+      'search/autosuggest/' + q + '.json', 
+      {
+        only: $(this).data('step'),
+        img_width: 50,
+        img_height: 50,
+        advanced: 1,
+        with_unvailable: 1
+      }, 
+      function(results){
+        container = $('#results-' + self.data('step') + ' ul');
+        console.log('scripts/user-start.js', 'callback', container);
+        for (k in results) {
+          title = typeof results[k].title != 'undefined' ? results[k].title : results[k].name;
+          container.append('<li>' + title + '<a data-id="'+results[k].id+'" rel="popover" data-placement="left" data-store-in-session="1" class="btn btn-suivre btn-plus fav-like" data-original-title="" title="">Ajouter à mes listes </a></li>')
+        }
+    });
+
+    return false;
+  })
 });
