6b0686a4fd8f9dd4659fdfcc48f6fb298d9e7cf9
axel@myskreen.com <ubuntu@ip-172-31-22-149.eu-west-1.compute.internal>
2014-05-27 16:03:54 +0000

    btn remove all notifications

--- Resources/public/js/scripts/_default.js
+++ Resources/public/js/scripts/_default.js
@@ -194,6 +194,8 @@ $(document).ready(function(){
 
   });
 
+  $('.notifications span.remove-all-notifs[rel="tooltip"]').tooltip();
+
   $(document).on('click', '.notifications .remove', function(e){
         e.preventDefault();
         Skhf.session.deleteNotification($(this).data('id'));
