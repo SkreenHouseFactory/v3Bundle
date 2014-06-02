4d31f491e19a2193fe0f81c7743ee150cb6ff9f9
axel@myskreen.com <ubuntu@ip-172-31-22-149.eu-west-1.compute.internal>
2014-06-02 07:46:27 +0000

    fix touch logos chaines

--- Resources/public/js/scripts/touch.js
+++ Resources/public/js/scripts/touch.js
@@ -9,10 +9,11 @@ $(document).ready(function(){
   	$('.carousel').hammer().on('swiperight', function(){
   		$(this).carousel('prev'); 
   	});
-    
+    $('.chaines-listing .logo_chaine').removeAttr('data-toggle data-original-title');
     
     //load beead
    /* console.log('scripts/touch.js', 'beead', 'load');
     $('body').append('<script type="text/javascript" src="http://as.ebz.io/api/choixPubJS.htm?pid=305331&screenLayer=1&mode=NONE&home=http://www.myskreen.com"></script>');*/
   }
+
 });
