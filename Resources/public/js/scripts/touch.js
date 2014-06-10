97e1554bff5abc0f90b262a401303f71460f8523
axel@myskreen.com <ubuntu@ip-172-31-22-149.eu-west-1.compute.internal>
2014-06-10 13:12:50 +0000

    fix double-click on program in tvgrid

--- Resources/public/js/scripts/touch.js
+++ Resources/public/js/scripts/touch.js
@@ -9,8 +9,12 @@ $(document).ready(function(){
   	$('.carousel').hammer().on('swiperight', function(){
   		$(this).carousel('prev'); 
   	});
+    // no tooltip on listing of tnt channels
     $('.chaines-listing .logo_chaine').removeAttr('data-toggle data-original-title');
-    
+    // handle no double-click on tv grid
+    $(document).on('mouseover mousemove', '.channel-wrap .nav-list a.link[data-ajax]', function(){
+      $(this).click();
+    });
     //load beead
    /* console.log('scripts/touch.js', 'beead', 'load');
     $('body').append('<script type="text/javascript" src="http://as.ebz.io/api/choixPubJS.htm?pid=305331&screenLayer=1&mode=NONE&home=http://www.myskreen.com"></script>');*/
