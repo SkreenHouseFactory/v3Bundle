c4bbd2bb49243bbfc75f833dea789183ed67b1d1
axel@myskreen.com <ubuntu@ip-172-31-22-149.eu-west-1.compute.internal>
2014-07-09 10:49:27 +0200

    fix bug in js for calling sticky kit on pages not calling original js

--- Resources/public/js/scripts/channel.js
+++ Resources/public/js/scripts/channel.js
@@ -71,23 +71,25 @@ $(document).ready(function(){
     }
 
     // Sticky for theaters
-    $('.view-fournisseur-cinema #theater-related').stick_in_parent({offset_top: 70})
-      .on('sticky_kit:bottom', function(e) {
-        $('#theater-related').parent().attr('style', function(i, style) {
-          return style.replace(/position[^;]+;?/g, '');
-        });
-        $('.view-fournisseur-cinema #theater-related').css({
-          position: 'absolute',
-          bottom: '0px',
-          top: 'auto'
-        });
-    });
-    // on page theater, modify sticky behaviour depending on display of bande_couleur_degrade
-    $(document).on('click', '[data-toggle-display]', function () {
-      setTimeout(function(){
-        $(document.body).trigger('sticky_kit:recalc');
-      }, 500);
-    });
+    if ($('.view-fournisseur-cinema #theater-related').length) {
+      $('.view-fournisseur-cinema #theater-related').stick_in_parent({offset_top: 70})
+        .on('sticky_kit:bottom', function(e) {
+          $('#theater-related').parent().attr('style', function(i, style) {
+            return style.replace(/position[^;]+;?/g, '');
+          });
+          $('.view-fournisseur-cinema #theater-related').css({
+            position: 'absolute',
+            bottom: '0px',
+            top: 'auto'
+          });
+      });
+      // on page theater, modify sticky behaviour depending on display of bande_couleur_degrade
+      $(document).on('click', '[data-toggle-display]', function () {
+        setTimeout(function(){
+          $(document.body).trigger('sticky_kit:recalc');
+        }, 500);
+      });
+    }
 
   // -- chaine
   } else if ($('#view-page').length) {
