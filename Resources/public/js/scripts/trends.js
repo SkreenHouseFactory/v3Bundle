e600cee6dae77424decc18184501a6c6931f01f8
axel@myskreen.com <ubuntu@ip-172-31-22-149.eu-west-1.compute.internal>
2014-08-12 17:08:42 +0200

    dev of trends page continue

--- Resources/public/js/scripts/trends.js
+++ Resources/public/js/scripts/trends.js
@@ -1,39 +1,106 @@
 var Trends;
 Trends = {
-  nb_weeks: 5,
-  list_programs: {},
+  nb_weeks: 10,
+  list_programs: [],
+  list_persons: [],
+  list_categories: [],
+  list_channels: [],
   init: function() {
-    var args = {};
+    Trends.getTrends();
+  },
+  getTrends: function(type,nb_weeks,args) {
+    var type = typeof type != 'undefined' ? '/' + type + '/' : '/';
+    var nb_weeks = typeof nb_weeks != 'undefined' ? nb_weeks : this.nb_weeks;
+    var args = typeof args != 'undefined' ? args : {};
     API.query(
       'GET',
-      'trends/' + this.nb_weeks + '.json',
+      'trends' + type + nb_weeks + '.json',
       args,
       function(datas){
-        console.log('scripts/trends.js', 'trends datas', datas);
-        console.log('scripts/trends.js', 'datas.program.weeks', datas.program.weeks);
-        for (i in datas.program.weeks) {
-          for (k in datas.program.weeks[i]) {
-            if (typeof this.list_programs == 'undefined') {
-              this.list_programs = new Object();
-              this.list_programs[k] = datas.program.weeks[i][k].titre;
-            } else if (typeof this.list_programs[k] == 'undefined') {
-              this.list_programs[k] = datas.program.weeks[i][k].titre;
-            }
-          }
+        if (type == '/') {
+          Trends.getList('/program/',datas,nb_weeks);
+          Trends.getList('/person/',datas,nb_weeks);
+          Trends.getList('/category/',datas,nb_weeks);
+          Trends.getList('/channel/',datas,nb_weeks);
+        } else {
+          Trends.getList(type,datas,nb_weeks);
         }
-        console.log('scripts/trends.js', 'list_programs', this.list_programs);
       }
     );
   },
-  draw: function(){
-    var r_programs = Raphael('holder');
-    var abs_programs = [];
-    var ord_programs = [];
-    var lines = r_programs.linechart(0,0,300,220,abs_programs,ord_programs)
+  getList: function(type,datas,nb_weeks){
+    switch (type) {
+      case '/program/':
+        var weeks = datas.program.weeks;
+        break;
+      case '/person/':
+        var weeks = datas.person.weeks;
+        break;
+      case '/category/':
+        var weeks = datas.category.weeks;
+        break;
+      case '/channel/':
+        var weeks = datas.channel.weeks;
+        break;
+    }
+    console.log('scripts/trends.js', 'trends datas', type, weeks);
+    var list = [];
+    for (i in weeks) {
+      for (k in weeks[i]) {
+        if (typeof list[k] == 'undefined') {
+          list[k] = [];
+          list[k]['titre'] = weeks[i][k].titre;
+        }
+      }
+    }
+    Trends.getCoord(type,weeks,list,nb_weeks);
+  },
+  getCoord: function(type,weeks,list,nb_weeks){
+    // console.log('scripts/trends.js', 'trends', type, 'list without Coords', list);
+    for (k in list) {
+      for (i in weeks) {
+        if (typeof weeks[i][k] != 'undefined') {
+          list[k][i] = weeks[i][k].nb;
+        } else {
+          list[k][i] = 0;
+        }
+      }
+    }
+    console.log('scripts/trends.js', 'trends list', type, 'list with Coords', list);
+    Trends.draw(type,list,nb_weeks);
+  },
+  draw: function(type,list,nb_weeks){
+    var type = type.replace('/','').replace('/','');
+    console.log('scripts/trends', 'draw', 'type', type);
+    var list_length = Object.keys(list).length;
+    console.log('scripts/trends', 'draw', 'list length', list_length);
+    var x_axis = [];
+    var i = 0;
+    for (k in list) {
+      x_axis[i] = [];
+      for (var j = 0; j < nb_weeks; j++) {
+        x_axis[i][j] = j+1;
+      };
+      i++;
+    }
+    console.log('scripts/trends', 'draw', 'x_axis complete', x_axis);
+    var y_axis = [];
+    var i = 0;
+    for (k in list) {
+      y_axis[i] = [];
+      for (var j = 0; j < list[k].length; j++) {
+        y_axis[i][j] = list[k][j];
+      }
+      i++;
+    }
+    console.log('scripts/trends', 'draw', 'y_axis complete', y_axis);
+    var r = Raphael(type);
+    var lines = r.linechart(30, 30, 450, 120, x_axis, y_axis, {
+      axis: '0 0 1 1'
+    });
   }
 }
 
 $(document).ready(function(){
   Trends.init();
-
 });
